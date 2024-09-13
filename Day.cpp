5b298ff1507957fb1402990f497f32bcd8515722
Tizianowu <tiziano.wu@edu.unifi.it>
2024-09-13 19:04:53 +0200

    Setting up Day class, and implementing its methods

--- Day.cpp
+++ Day.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Day.h"
+int Data::giorniNelMese(int mese, int anno) const {
+    // Array dei giorni per ciascun mese (0-esimo indice inutilizzato per semplicità)
+    int giorniPerMese[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    if (mese == 2 && isBisestile(anno)) {
+        return 29;
+    }
+    if (mese >= 1 && mese <= 12) {
+        return giorniPerMese[mese - 1];
+    }
+    return 0;
+}
+
+bool Data::dataValida(int giorno, int mese, int anno) const{
+    if (anno < 1 || mese < 1 || mese > 12) {
+        return false;
+    }
+    int maxGiorni = giorniNelMese(mese, anno);
+    return (giorno >= 1 && giorno <= maxGiorni);
+}
+
+bool Data::operator==(const Data &altraData) const {
+    return (day == altraData.day &&
+            month == altraData.month &&
+            year == altraData.year);
+}
+
--- Day.h
+++ Day.h
@@ -1,14 +1,42 @@
 //
 // Created by Tiziano Wu on 13/09/24.
 //
+#ifndef REGISTRO_GIORNO_DAY_H
+#define REGISTRO_GIORNO_DAY_H
+#include <iostream>
 
-#ifndef REGISTRO_ATTIVITA_DAY_H
-#define REGISTRO_ATTIVITA_DAY_H
+class Data {
+private:
+    int day;
+    int month;
+    int year;
+public:
+    Data() : day(1), month(1), year(1970) {};
+    Data(int giorno, int mese, int anno) {
+        if (dataValida(giorno, mese, anno)) {
+            this->day = giorno;
+            this->month = mese;
+            this->year = anno;
+        } else {
+            std::cerr << "Errore: data non valida!" << std::endl;
+        }
+    }
 
+    bool isBisestile(int anno) const {
+        return ((anno % 4 == 0 && anno % 100 != 0) || (anno % 400 == 0));}
+    int giorniNelMese(int mese, int anno) const;
+    bool dataValida(int giorno, int mese, int anno) const;
+    bool operator==(const Data& altraData) const;
 
-class Day {
-
+    int getGiorno() const {
+        return day;
+    }
+    int getMese() const {
+        return month;
+    }
+    int getAnno() const {
+        return year;
+    }
 };
 
-
-#endif //REGISTRO_ATTIVITA_DAY_H
+#endif //REGISTRO_GIORNO_DAY_H
